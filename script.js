62bbdf79740bc698b2a7bf2e6634672060e7ac0b
javastart <nigmatov2508@gmail.com>
2022-11-20 10:55:11 +0500

    java start

---
+++ script.js
@@ -0,0 +1,58 @@
+//var  name = prompt('Ввести свое имя')
+//console.log( 'вас зовут: ' + name  );
+//var age = +prompt('Ввести возраст');
+//console.log('ваш возраст ' + age);
+//var  slog = +prompt('6 + 3 =');
+//console.log('6 + 3 = ' + slog + ' Ответ: ' + (6 + 3));
+//var  slog2 = +prompt('6 - 3 =');
+//console.log('6 - 3 = ' + slog2 + ' Ответ: ' + (6 - 3));
+//var  slog3 = +prompt('6 / 5 =');
+//console.log('6 / 5 = ' + slog3+ ' Ответ: ' + (6 / 5)); 
+//var  slog = +prompt('6 * 3 =');
+//console.log('6 * 3 = ' + slog + ' Ответ: ' + (6 * 3));
+//alert('посмотри в консоль') ;
+
+
+//var age = +prompt('введите возраст')
+
+//if (age > 0 && age <= 18 ) {
+ //   alert('Вы еще молоды. Вам нужно учится');
+//}else if (age > 18 && age <= 50 ) {
+//    alert('Вам нужно работать') ;
+//}else if (age > 50 && age <= 59 ) {
+ //   alert('Вам скоро на пенсию') ;
+//}else if (age > 59 && age <= 150 ) {
+//    alert('Вы на пенсии') ;
+//}else if (isNaN) {
+ //   alert('что-то пошло не так')
+//}
+
+//var t = +prompt('вести время')
+
+//if (0 < t && t <= 4) {
+//    alert( t + ' часа ночи');
+//}else if (4 <= t && t <= 11 ){
+ //   alert( t + ' часа утра');
+//}else if ( 11 < t && t <= 16){
+//    alert( t + ' часа дня');
+//}else if ( 16 < t && t <= 23){
+//    alert( t + 'часа вечера');
+//}
+
+//var a = +prompt('первое число')
+//var b = +prompt('второе число')
+//var c = +prompt('третье число')
+
+//if ( a < b < c) {
+//    alert( b + ' среднее число')
+//}else if ( b < c < a) {
+//    alert( c + ' среднее число')
+//}else if ( c < a < b) {
+//    alert( a + ' среднее число')
+//}else if ( c < b < a) {
+//    alert( b + 'среднее число')
+//}else if ( a < c < b) {
+//    alert( c + 'среднее число')
+//}else if ( b < a < c) {
+//    alert( a + 'среднее число')
+//}
